d6e7fdf5df1d65920b8cac6373ea45e9c49c059f
CJ Virtucio <cjv287@gmail.com>
2023-12-28 00:33:31 -0500

    removed cruft; some logging

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,50 +2,23 @@
 
 set -xeuo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for ansible.
-GH_REPO="https://github.com/ansible/ansible"
-GH_API_REPO="https://api.github.com/repos/ansible/ansible"
-
 fail() {
   echo -e "asdf-ansible: $*"
   exit 1
 }
 
-curl_opts=(-fsSL)
-
-# NOTE: You might want to remove this if ansible is not hosted on GitHub releases.
-# if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-#   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-# fi
-
-sort_versions() {
-  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
-}
-
-list_github_tags() {
-  #   git ls-remote --tags --refs "$GH_REPO" |
-  #     grep -o 'refs/tags/.*' | cut -d/ -f3- |
-  #     sed 's/^v//' | grep -E '^[0-9a-z\.\-]+' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+list_ansible_versions() {
   curl -L -s 'https://pypi.org/pypi/ansible/json' | jq  -r '.releases | keys | .[]' | sort -V
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if ansible has other means of determining installable versions.
-  list_github_tags
+  list_ansible_versions
 }
 
-download_release() {
-  local version filename url
-  version="$1"
-  filename="$2"
-
-  # TODO: Adapt the release URL convention for ansible
-  url="$GH_REPO/archive/v${version}.tar.gz"
-
-  echo "* Downloading ansible release $version..."
-  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+log() {
+  local ts
+  ts="$(date -Is)"
+  >&2 printf '[%s] %s\n' "${ts}" "$1"
 }
 
 install_version() {
@@ -58,25 +31,15 @@ install_version() {
   fi
 
   if [ "$version" == 'latest' ]; then
-    version=$(list_github_tags | tail -n1)
-    # version=$(curl -s "$GH_API_REPO/releases/latest" | jq '.tag_name' -r | sed 's/^v//')
+    version=$(list_ansible_versions | tail -n1)
   fi
 
-  # TODO: Adapt this to proper extension and adapt extracting strategy.
-  local release_file="$install_path/ansible-$version.tar.gz"
+  local pip_path
+  pip_path="$(command -v pip)"
+  log "ansible will be installed using the following pip: ${pip_path}"
+
   (
-    # mkdir -p "$install_path"
-    # download_release "$version" "$release_file"
-    # tar -xzf "$release_file" -C "$install_path" --strip-components=1 || fail "Could not extract $release_file"
-    # rm "$release_file"
     pip install ansible=="$version"
-
-    # TODO: Asert ansible executable exists.
-    local tool_cmd
-    tool_cmd="$(echo "ansible --version" | cut -d' ' -f1)"
-    test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
-
-    test $("$install_path/bin/$tool_cmd" --version | grep -E '^ansible [0-9a-z\.\-]+' | cut -d ' ' -f 2) == "$version"
 
     echo "ansible $version installation was successful!"
   ) || (
